3b7a7b420251694f83d7e8d3784f69250d9e990a
r1ck7 <richims026@gmail.com>
2025-09-17 09:18:57 -0500

    Modificar con Select

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+List<User> mylist = new();
+
+for (int i = 1; i <= 10; i++)
+{
+  mylist.Add(new User(i, "", Random.Shared.NextDouble(), true));
+}
+mylist.ForEach(x => Console.WriteLine($"{x.Score} - {x.Check}"));
+
+Console.WriteLine("Despues del cambio");
+
+mylist.Select(i => i with { Check = i.Score < 0.5 ? true : false })
+      .ToList()
+      .ForEach(x => Console.WriteLine($"{x.Score} - {x.Check}"));
+
+public record User(int Id, string? Nap, double Score, bool Check);
